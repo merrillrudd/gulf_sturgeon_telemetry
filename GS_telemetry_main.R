47e6bafbc41734b44bbedb67899d7bd6225c7673
merrillrudd <merrillrudd@gmail.com>
2015-12-04 10:42:06 -0800

    separate analyses by river or region

--- GS_telemetry_main.R
+++ GS_telemetry_main.R
@@ -25,20 +25,25 @@ caphist <- setup_capture_histories(data=GSdets, tags=tags)
 ## convert monthly to 2-season time scale
 caphist2 <- months2seasons(ch=caphist, num_seasons=2) ## also option for 4 seasons per year
 
-## convert all rivers to 3 states (focal, river, marine) - list of capture history matrix for each of nine rivers
-river_single <- c("S", "K", "A", "C", "Y", "B", "E", "P", "L")
-ch_focal <- lapply(1:length(river_single), function(x) convert_states(ch=caphist2, focal=river_single[x]))
-names(ch_focal) <- river_single
+######## ANALYSIS BY RIVER
+    ## convert all rivers to 3 states (focal, river, marine) - list of capture history matrix for each of nine rivers
+    river_single <- c("S", "K", "A", "C", "Y", "B", "E", "P", "L")
+    group_single <- c("A", "B", "C", "D")    
 
-## create Rmark-ready capture histories
-ch_rmark <- lapply(1:length(ch_focal), function(x) make_ch_MARK(ch=ch_focal[[x]], focal=names(ch_focal[x]), tags=tags))
-names(ch_rmark) <- names(ch_focal)
+    ch_focal <- lapply(1:length(river_single), function(x) convert_states(ch=caphist2, spatial_collapse="river", focal=river_single[x], tags=tags))
+    names(ch_focal) <- river_single    
+
+    ## create Rmark-ready capture histories
+    ch_rmark <- lapply(1:length(ch_focal), function(x) make_ch_MARK(ch=ch_focal[[x]], focal=names(ch_focal[x]), tags=tags))
+    names(ch_rmark) <- names(ch_focal)
+
+
+####### ANALYSIS BY REGION
+    ch_group <- convert_states(ch=caphist2, spatial_collapse="region")
+
 
 ## run Rmark
-## can accomodate:
-	## surv == "stratum"
-	## cap == "constant" or "season"
-	## move == "random", "natal", "random_fixOut" or "natal_fixOut"
+
 
 ## check assumptions
 ## check assumption that fish do not move between river drainages in each season
--- R_functions/convert_states.R
+++ R_functions/convert_states.R
@@ -1,4 +1,4 @@
-convert_states <- function(ch, focal){
+convert_states <- function(ch, focal=NULL, tags=NULL, spatial_collapse){
 
 	convert_ch <- matrix(NA, nrow=nrow(ch), ncol=ncol(ch))
 		colnames(convert_ch) <- colnames(ch)
@@ -17,20 +17,69 @@ convert_states <- function(ch, focal){
 
 	indexR <- (1:ncol(ch))[-indexM]
 
-	convertR <- function(col, focal){
-		col[which(col!=focal & col!="0")] <- "R"
+	# convertRiver <- function(col, focal){
+	# 	col[which(col!=focal & col!="0")] <- "R"
+	# 	return(col)
+	# }
+
+	convertRegion <- function(col){
+		col[which(col=="S")] <- 1
+		col[which(col=="K")] <- 1
+		col[which(col=="A")] <- 1
+		col[which(col=="C")] <- 2
+		col[which(col=="Y")] <- 3
+		col[which(col=="B")] <- 3
+		col[which(col=="E")] <- 3
+		col[which(col=="P")] <- 4
+		col[which(col=="L")] <- 4
+
+		col[which(col==1)] <- "A"
+		col[which(col==2)] <- "B"
+		col[which(col==3)] <- "C"
+		col[which(col==4)] <- "D"
+
 		return(col)
 	}
 
-	for(i in indexR){
-		convert_ch[,i] <- convertR(col=ch[,i], focal=focal)
-	}
+	if(spatial_collapse=="river"){
 
-	if(any(convert_ch[,indexM] %in% c("R", focal))) stop("riverine state in marine season")
-	if(any(convert_ch[,indexR]=="M")) stop("marine state in riverine season")
-	if(any(is.na(convert_ch))) stop("not all seasons converted properly")
-	if(any(convert_ch %in% c("0", "M", "R", focal) == FALSE)) stop("error converting states")
+    	# for(i in indexR){
+    	# 	convert_ch[,i] <- convertRiver(col=ch[,i], focal=focal)
+    	# }    
 
-	return(convert_ch)
+    	# if(any(convert_ch[,indexM] %in% c("R", focal))) stop("riverine state in marine season")
+    	
+		for(i in indexR){
+			convert_ch[,i] <- ch[,i]
+		}
+
+		if(length(focal)==0 | length(tags)==0) stop("must specify focal river and transmitter-river matching matrix when spatial_collapse==river")
+
+    	if(any(convert_ch[,indexR]=="M")) stop("marine state in riverine season")
+    	if(any(is.na(convert_ch))) stop("not all seasons converted properly")
+
+    	river_double <- c("SR", "OR", "AR", "CR", "YR", "BR", "ER", "PR", "PE")
+    	river_single <- c("S", "K", "A", "C", "Y", "B", "E", "P", "L")
+
+    	sub_tags <- tags$Transmitter[which(tags$River==river_double[which(river_single==focal)])]
+
+    	ch_out <- convert_ch[which(rownames(convert_ch) %in% sub_tags),]
+    }
+
+    if(spatial_collapse=="region"){
+
+    	for(i in indexR){
+    		convert_ch[,i] <- convertRegion(col=ch[,i])
+    	}
+
+    	if(any(convert_ch[,indexM] %in% c("A", "B", "C", "D"))) stop("riverine state in marine season")
+    	if(any(convert_ch[,indexR]=="M")) stop("marine state in riverine season")
+    	if(any(is.na(convert_ch))) stop("not all seasons converted properly")
+    	if(any(convert_ch %in% c("0", "M", "A", "B", "C", "D") == FALSE)) stop("error converting states")    
+
+    	ch_out <- convert_ch
+    }
+
+	return(ch_out)
 
 }
